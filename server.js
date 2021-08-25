3eda315c57f2a654e27ee1d538b8cb7570da7047
Ryan Emmans <ryanemmans@gmail.com>
2021-08-25 10:37:53 -0700

    Broken code, work in progress.

--- server.js
+++ server.js
@@ -9,40 +9,45 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (request, response) => {
-  response.send('hello world');
-});
+// app.get('/', (request, response) => {
+//   response.send('hello world');
+// });
 
 app.get('/weather', (request, response) => {
+  const lat = request.query.lat;
+  const lon = request.query.lon;
   let searchQuery = request.query.cityName;
-  // console.log('SEARCHQUERY', searchQuery);
-  const city = weather.find(city => city.city_name === searchQuery);
+  const cityObject = weather.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+  const forecast = new Forecast(cityObject);
+  response.send([forecast])
+  // response.send([lat, lon, searchQuery])
   // console.log('SEARCHQUERY', city);
   // const city = weather.find(city => {
   //   console.log(city.city_name);
   // });
 
-  if (city) {
-    const weather = city.data.map(day => new Forecast(day));
+  if (forecast) {
+    const weather = forecast.data.map(day => new Forecast(day));
     response.send(weather);
   } else {
-    response.send('No city found.');
+    response.status(500).send('No city found.');
     // throw new Error('Invalid');
     // response.status(400).send('No city found.');
   }
 });
 
-function Forecast(day) {
-  this.date = day.valid_date;
-  this.description = `Low of ${day.low_temp}, High of ${day.high_temp}, with ${day.weather.description}`;
-}
+class Forecast {
+  constructor (data) {
+  this.date = data(valid_date);
+  this.description = data(`Low of ${low_temp}, High of ${high_temp}, with ${weather.description}`);
+}}
 
-function error(err, req, res, next) {
-  console.log(err.stack);
-  res.status(500);
-  res.send({ error: "Something went wrong."});
-}
+// function error(err, req, res, next) {
+//   console.log(err.stack);
+//   res.status(500);
+//   res.send({ error: "Something went wrong."});
+// }
 
-app.use(error);
+// app.use(error);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
