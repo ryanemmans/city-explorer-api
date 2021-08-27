a7d1c581028e50158e11a5b4d5de84f27c326f11
Ryan Emmans <ryanemmans@gmail.com>
2021-08-26 18:25:13 -0700

    Adds weather API to front-end. Creates new module files.

--- server.js
+++ server.js
@@ -2,50 +2,50 @@
 
 const express = require('express');
 const cors = require('cors');
-const weatherData = require('./data/weather.json');
 const axios = require('axios');
 require('dotenv').config();
+// const weatherData = require('./data/weather.json');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+app.get('/weather', async (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  // const searchQuery = req.query.query;
+  // const cityObject = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+
+  // if (cityObject) {
+  //   res.status(200).send(weather);
+  // } else {
+  try {
+    const weatherKey = process.env.WEATHER_API_KEY;
+    const weather_API_URL = `https://api.weatherbit.io/v2.0/forecast/daily/?key=${weatherKey}&lat=${lat}&lon=${lon}&lang=en&days=3&units=I`;
+
+    const weatherAPIRes = await axios.get(weather_API_URL);
+    console.log(weatherAPIRes.data.data);
+    const weather = weatherAPIRes.data.data.map(day => new Forecast(day));
+    console.log('LOOK AT ME!!', weather);
+    res.send(weather);
+
+    // const weatherArray = weatherAPIRes.data.data.map((day) => new Forecast(day)
+    // );
+
+    // res.status(200).send(weatherArray);
+  } catch (error) {
+    res.status(500).send('No city found.');
+  }
+})
+// });
+
 class Forecast {
   constructor(day) {
-    this.date = day.valid_date;
-    this.description = `Low of ${day.low_temp}, High of ${day.high_temp}, with ${day.weather.description}`;
+    this.date = day.datetime;
+    this.dailyForecast = `High of ${day.high_temp}, Low of ${day.low_temp}, with ${day.weather.description}`;
   }
 }
-
-app.get('/weather', async (req, res) => {
-  const latQuery = req.query.lat;
-  const lonQuery = req.query.lon;
-  const searchQuery = req.query.cityName;
-  // const searchQuery = req.query.query;
-  const cityObject = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
-
-  if (cityObject) {
-    const weather = cityObject.data.map(day => new Forecast(day));
-    res.status(200).send(weather);
-  } else {
-    try {
-      const weatherKey = process.env.WEATHER_API_KEY;
-      const weatherAPI_URL = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${weatherKey}&city=${searchQuery}$lat=${latQuery}&lon=${lonQuery}&lang=en&days=3&format=json`;
-
-      const weatherAPIRes = await axios.get(weatherAPI_URL);
-
-      // const weatherArray = weatherAPIRes.data.data.map((day) => new Forecast(day)
-      // );
-
-      // res.status(200).send(weatherArray);
-    } catch (error) {
-      res.status(500).send('No city found.');
-    }
-  }
-});
-
-
 
 // function error(err, req, res, next) {
 //   res.status(500);
